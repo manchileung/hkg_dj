5989140a5f0b2e25abdd286e608723b9761dd8a4
manchileung <alvin1.22006@gmail.com>
2013-11-03 02:45:40 +0800

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -171,7 +171,7 @@ function displayUI()
   $('#plugbot-ui').append('<p id="plugbot-btn-hidevideo" style="color:' + cHideVideo
     + '">摺埋首歌</p><p id="plugbot-btn-skipvideo" style="color:' + BUTTON_OFF + '">摺埋+靜音</p>'
     + '<p id="plugbot-btn-userlist" style="color:' + cUserList 
-    + '">用戶列表</p>);
+    + '">用戶列表</p>');
 
 }
 
