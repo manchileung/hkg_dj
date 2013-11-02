e04862a17abb54456c6b13b9e784748f76efc314
manchileung <alvin1.22006@gmail.com>
2013-11-03 05:18:53 +0800

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -129,17 +129,7 @@ function initAPIListeners()
 
 /**
  * Renders all of the Plug.bot "UI" that is visible beneath the video player.
- */
-function displayiconUI()
-{
-if iconnnn{
-$('#plugbot-icon').remove();
-$('#footer-container').prepend('<div id="plugbot-icon"></div>');
-$('#plugbot-icon').append('<iframe src="http://www.emoji-cheat-sheet.com/" border="0" height="700px" width="1100px"></frame>');
-}else{
- $('#plugbot-icon').remove();
-}
-}
+
  
 function displayUI()
 {
@@ -176,7 +166,7 @@ function displayUI()
   $('#plugbot-ui').append('<p id="plugbot-btn-hidevideo" style="color:' + cHideVideo
     + '">摺埋首歌</p><p id="plugbot-btn-skipvideo" style="color:' + BUTTON_OFF + '">摺埋+靜音</p>'
     + '<p id="plugbot-btn-userlist" style="color:' + cUserList 
-    + '">用戶列表</p><p><span>EMOJI ICON</p>');
+    + '">用戶列表</p><p><span onclick="$iconnnn">EMOJI ICON</p>');
 
 }
 
