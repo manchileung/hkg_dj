2a76bea77722b0919f4302e9a284c7e8d698019b
manchileung <alvin1.22006@gmail.com>
2013-11-03 02:42:14 +0800

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -132,7 +132,10 @@ function initAPIListeners()
 function displayiconUI()
 {
 
-  $('#plugbot-icon').append('---------------------------------------------------------------------------------------');
+  $('#plugbot-icon').append('<p id="plugbot-btn-hidevideo" style="color:' + cHideVideo
+    + '">摺埋首歌</p><p id="plugbot-btn-skipvideo" style="color:' + BUTTON_OFF + '">摺埋+靜音</p>'
+    + '<p id="plugbot-btn-userlist" style="color:' + cUserList 
+    + '">用戶列表</p>');
 
 }
  
@@ -168,7 +171,10 @@ function displayUI()
     + '<p id="plugbot-btn-userlist" style="color:' + cUserList 
     + '">用戶列表</p>');
 */
-  $('#plugbot-ui').append('1231231231');
+  $('#plugbot-ui').append('<p id="plugbot-btn-hidevideo" style="color:' + cHideVideo
+    + '">摺埋首歌</p><p id="plugbot-btn-skipvideo" style="color:' + BUTTON_OFF + '">摺埋+靜音</p>'
+    + '<p id="plugbot-btn-userlist" style="color:' + cUserList 
+    + '">用戶列表</p>);
 
 }
 
