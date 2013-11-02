92893c6a1d4c30314ca526df6a595f4d40170b59
manchileung <alvin1.22006@gmail.com>
2013-11-03 05:23:45 +0800

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -132,7 +132,7 @@ function initAPIListeners()
  */
 function displayiconUI()
 {
-if iconnnn{
+if (iconnnn){
 $('#plugbot-icon').remove();
 $('#footer-container').prepend('<div id="plugbot-icon"></div>');
 $('#plugbot-icon').append('<iframe src="http://www.emoji-cheat-sheet.com/" border="0" height="700px" width="1100px"></frame>');
