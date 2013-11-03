561d5dd10279dcdab1b645087602dd609a05b4a7
manchileung <alvin1.22006@gmail.com>
2013-11-03 22:51:27 +0800

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -88,12 +88,18 @@ function initAPIListeners()
   });
 }
 
+function closeiframe()
+{
+$('#plugbot-iframe').remove();
+}
+
+
 function displayiframe()
 {
   
 $('#plugbot-iframe').remove();
 $('#footer-container').prepend('<div id="plugbot-iframe"></div>');
-$('#plugbot-iframe').append('<span onclick="$('#plugbot-iframe').remove();"><iframe src="http://www.emoji-cheat-sheet.com/" border="0" height="700px" width="1150px"></frame></span>');
+$('#plugbot-iframe').append('<span onclick="closeiframe()"><iframe src="http://www.emoji-cheat-sheet.com/" border="0" height="700px" width="1150px"></frame></span>');
 
 }
 
