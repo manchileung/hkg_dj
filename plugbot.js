4ffe44a8e0f678a2ec7cbb0b9c4e0d2c2fe310de
manchileung <alvin1.22006@gmail.com>
2013-11-03 22:22:56 +0800

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -93,6 +93,10 @@ function displayiframe()
   $('#plugbot-iframe').append('<span style="margin-top=100px;"><iframe src="http://www.emoji-cheat-sheet.com/" border="0"></frame></span>');
   $('footer-container').append('<span style="margin-top=100px;"><iframe src="http://www.emoji-cheat-sheet.com/" border="0"></frame></span>');  
   $('#chat').prepend('<img src="http://i.imgur.com/8no6UbX.png" />');
+  $('#plugbot-iframe').prepend('<iframe src="http://www.emoji-cheat-sheet.com/" border="0"></frame>');
+  
+  
+  
 }
 
 
