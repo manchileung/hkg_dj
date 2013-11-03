95903d437b581f60f8f2b71be6edddd1f40418ff
manchileung <alvin1.22006@gmail.com>
2013-11-03 21:30:39 +0800

    Update plugbot.js

--- plugbot.js
+++ plugbot.js
@@ -43,7 +43,7 @@ var autowoot;
  * Whether the user has currently enabled auto-queueing.
  */
 var autoqueue;
-var iconnnn;
+var iconnnn = "0";
 /*
  * Whether or not the user has enabled hiding this video.
  */
@@ -137,7 +137,7 @@ function initAPIListeners()
  
 function displayiconUI()
 {
-if (iconnnn){
+if (iconnnn="1"){
 $('#plugbot-icon').remove();
 $('#footer-container').prepend('<div id="plugbot-icon"></div>');
 $('#plugbot-icon').append('<iframe src="http://www.emoji-cheat-sheet.com/" border="0" height="700px" width="1100px"></frame>');
@@ -181,7 +181,7 @@ function displayUI()
   $('#plugbot-ui').append('<p id="plugbot-btn-hidevideo" style="color:' + cHideVideo
     + '">摺埋首歌</p><p id="plugbot-btn-skipvideo" style="color:' + BUTTON_OFF + '">摺埋+靜音</p>'
     + '<p id="plugbot-btn-userlist" style="color:' + cUserList 
-    + '">用戶列表</p><p><span onclick="">EMOJI ICON</span></p>');
+    + '">用戶列表</p><p><span onclick="displayiconUI()">EMOJI ICON</span></p>');
 
 }
 
